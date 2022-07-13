91ca3fb61053395f66da0226481bad0cc38b6b85
Leo-Xee <jang.xmin@gmail.com>
2022-07-13 18:56:20 +0900

    feat: react v18 대응

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,9 @@
 import React from "react";
-import ReactDom from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./App";
 
-ReactDom.render(<App />, document.getElementById("root"));
+const element = document.getElementById("root");
+if (!element) throw Error("Root container missing in index.html");
+
+const root = ReactDOM.createRoot(element);
+root.render(<App />);
